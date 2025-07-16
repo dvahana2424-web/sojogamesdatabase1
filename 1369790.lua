bcfbe9e42dbe7ba5621863e77021fbf987729c29
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 02:58:20 +0800

    Upload via drag-and-drop tool

---
+++ 1369790.lua
@@ -0,0 +1,6 @@
+-- MAIN APPLICATION
+addappid(1369790) -- Marshmallow All the Way Home
+
+-- MAIN APP DEPOTS
+addappid(1369791, 1, "5b55f191681a5d180ad1d19d09745311f45aa0182053aa3fb6acc70bebab39af") -- Depot 1369791
+setManifestid(1369791, "5215147001386860409", 0)

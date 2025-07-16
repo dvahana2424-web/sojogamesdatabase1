10367de03bfe6c5522c1ef5d1dd96b6e94cd620c
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 03:09:30 +0800

    Upload via drag-and-drop tool

---
+++ 2251400.lua
@@ -0,0 +1,6 @@
+-- MAIN APPLICATION
+addappid(2251400) -- Everdeep Aurora
+
+-- MAIN APP DEPOTS
+addappid(2251401, 1, "764d3b25a6137d9b57a29b8b0c012befe232474a74ad6789a8dba423bed4b5a5") -- Depot 2251401
+setManifestid(2251401, "2344586265573538068", 0)

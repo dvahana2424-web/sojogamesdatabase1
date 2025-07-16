63c6b6cd4dec073b4178690c8604602b600e7cff
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 02:54:17 +0800

    Upload via drag-and-drop tool

---
+++ 3415580.lua
@@ -0,0 +1,6 @@
+-- MAIN APPLICATION
+addappid(3415580) -- Death Ring: Second Impact 
+
+-- MAIN APP DEPOTS
+addappid(3415581, 1, "e5ce809817aa13012ec22793cafe8d2d4b1d1fb7306e8a4b0c32e4e8dde66b89") -- Depot 3415581
+setManifestid(3415581, "1438659573378302154", 0)

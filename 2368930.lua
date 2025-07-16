8bd75b2506192f267885b6ed4ad2d7e84265e481
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 01:37:20 +0800

    Add files via upload

---
+++ 2368930.lua
@@ -0,0 +1,21 @@
+-- Generated Lua Manifest by Morrenus
+-- Steam App 2368930 Manifest
+-- Name: ISLANDERS: New Shores
+-- Generated: 2025-07-16 19:36:29
+-- Total Depots: 1
+-- Total DLCs: 1 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2368930) -- ISLANDERS: New Shores
+
+-- MAIN APP DEPOTS
+addappid(2368931, 1, "924f9db27c7400a1185ba793bbd54719c53dd4c3e1e55aaeb340ab7a34bfd73f") -- Depot 2368931
+setManifestid(2368931, "2922232987347956321", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3508640) -- Filters DLC
+
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- ISLANDERS New Shores Soundtrack (AppID: 3478830) - missing depot keys
+-- addappid(3478830)

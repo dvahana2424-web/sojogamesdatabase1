f6b13c691f41a8b0056fc8131aa3c45f85ad4eeb
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 13:15:25 +0800

    Upload via drag-and-drop tool

---
+++ 2225960.lua
@@ -0,0 +1,8 @@
+-- Name: Noobs Are Coming
+
+
+addappid(2225960) -- Noobs Are Coming
+
+
+addappid(2225961, 1, "b35dd338e7aa221d314ac1f95cfa3fe531cb6c5d8bd668be47c7a8bbb1485ad9") -- Depot 2225961
+-- setManifestid(2225961, "4143147174134424806", 0)

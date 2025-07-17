c98ccdbedfe223a5e79893c6c83a87c77928500c
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 22:44:05 +0800

    Upload via drag-and-drop tool

---
+++ 3199430.lua
@@ -0,0 +1,12 @@
+-- MAIN APPLICATION
+addappid(3199430) -- Eyes on the Exam
+
+-- MAIN APP DEPOTS
+addappid(3199431, 1, "6931e9e96b1524bca005ecfa980ae533aedd7795d7bc6824ac2eb007c819b8af") -- Depot 3199431
+setManifestid(3199431, "3941688492232542389", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)

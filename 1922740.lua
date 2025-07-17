cc9a1396b09cc661728f47651291a76e9d6349f4
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 14:46:02 +0800

    Upload via drag-and-drop tool

---
+++ 1922740.lua
@@ -0,0 +1,15 @@
+-- MAIN APPLICATION
+addappid(1922740) -- Crisis in the Kremlin: The Cold War
+
+-- MAIN APP DEPOTS
+addappid(1922741, 1, "e7bf3b11e7458955033fd3b50bf48cc2b0c91c7bede7c8004b40e041e8eb5f9e") -- Depot 1922741
+setManifestid(1922741, "4565342030756130227", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Crisis in the Kremlin The Cold War - Patron Edition (AppID: 3637930) - missing depot keys
+-- addappid(3637930)

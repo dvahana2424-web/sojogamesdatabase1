8534402f872f216d1bbc2f24c0bf076f84f90ad6
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 14:48:05 +0800

    Upload via drag-and-drop tool

--- 2147530.lua
+++ 2147530.lua
@@ -1,5 +1,17 @@
-addappid(2147530)
-addappid(228989)
-setManifestid(228989,"1332597174812030948")
-addappid(2147531,0,"fb31e66f583fb6609b0587b988db6c21f560cf9e7efd00abf3446ce0bc80eede")
-setManifestid(2147531,"4291709477091828517")
+-- MAIN APPLICATION
+addappid(2147530) -- Raining Blood: Hellfire
+
+-- MAIN APP DEPOTS
+addappid(2147531, 1, "fb31e66f583fb6609b0587b988db6c21f560cf9e7efd00abf3446ce0bc80eede") -- Depot 2147531
+setManifestid(2147531, "9099840049920707857", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 0)
+
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Raining Blood Hellfire Soundtrack (AppID: 3844990) - missing depot keys
+-- addappid(3844990)
+-- Raining Blood Hellfire - Pacote Yodismo (AppID: 3869040) - missing depot keys
+-- addappid(3869040)

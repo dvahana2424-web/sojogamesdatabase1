8b88691cfbd1c22684b2593bdc7cc5ce37159b82
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 21:54:57 +0800

    Upload via drag-and-drop tool

---
+++ 3587090.lua
@@ -0,0 +1,10 @@
+-- MAIN APPLICATION
+addappid(3587090) -- Double It
+
+-- MAIN APP DEPOTS
+addappid(3587091, 1, "2e3e3a58cdd70e1badc4b5d64dba857d13ef44f43866bcf1b75e667748784535") -- Depot 3587091
+setManifestid(3587091, "6140335102012457750", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)

17d3e396132516f5c8e7ba96dc21d7f9481700a0
sojorepo <thepoorgamer2424@gmail.com>
2025-07-18 01:28:06 +0800

    Upload via drag-and-drop tool

---
+++ 3444080.lua
@@ -0,0 +1,12 @@
+-- MAIN APPLICATION
+addappid(3444080) -- Bakery Cafe Simulator
+
+-- MAIN APP DEPOTS
+addappid(3444081, 1, "11ced1ac7e5218140e0c3802ac74011ca3d2ccc2979f5be29b709cf33ef1175b") -- Depot 3444081
+setManifestid(3444081, "8957773130712454638", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)

57556e4604dc5b723aa0de6378fc3eb3c7856f8d
sojorepo <thepoorgamer2424@gmail.com>
2025-07-17 21:50:18 +0800

    Upload via drag-and-drop tool

--- 1646790.lua
+++ 1646790.lua
@@ -1,3 +1,11 @@
-addappid(1646790)
-addappid(1646791,0,"387a839d535759a41e628d9eae51b5df05ad03e45ce39e9d92c6a9bd110d9bd5")
-setManifestid(1646791,"1015309769119956453")
+-- MAIN APPLICATION
+addappid(1646790) -- Striving for Light
+
+-- MAIN APP DEPOTS
+addappid(1646791, 1, "387a839d535759a41e628d9eae51b5df05ad03e45ce39e9d92c6a9bd110d9bd5") -- Striving for Light Content
+setManifestid(1646791, "7816544722434525358", 0)
+
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Striving for Light Soundtrack (AppID: 1829720) - missing depot keys
+-- addappid(1829720)

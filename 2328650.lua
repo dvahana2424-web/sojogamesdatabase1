d0ca135be49152ccb109ccba442f3924cd1f76df
sojorepo <thepoorgamer2424@gmail.com>
2025-07-18 00:49:25 +0800

    Upload via drag-and-drop tool

--- 2328650.lua
+++ 2328650.lua
@@ -1,3 +1,6 @@
-addappid(2328650)
-addappid(2328651, 1, "b9e753aca4fea95b65712e3789ec37b64c4d7fa62a3f8f026e342f8085b64e3d")
-setManifestid(2328651, "6154157427105734049", 0)
+-- MAIN APPLICATION
+addappid(2328650) -- Locomoto
+
+-- MAIN APP DEPOTS
+addappid(2328651, 1, "b9e753aca4fea95b65712e3789ec37b64c4d7fa62a3f8f026e342f8085b64e3d") -- Main Game Content (Windows Content)
+setManifestid(2328651, "3225499998782740819", 0)

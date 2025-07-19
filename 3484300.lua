c5d66fcea113edb18009f69fad79670c6977dfcb
sojorepo <thepoorgamer2424@gmail.com>
2025-07-19 19:20:14 +0800

    Upload via drag-and-drop tool

---
+++ 3484300.lua
@@ -0,0 +1,12 @@
+-- MAIN APPLICATION
+addappid(3484300) -- DREADZONE
+
+-- MAIN APP DEPOTS
+addappid(3484301, 1, "e26f07fd4b7fdee8af7f179b8f498c0527b56121cc06ecfa93a2b3c2c683e226") -- Depot 3484301
+setManifestid(3484301, "1534192719550948162", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)

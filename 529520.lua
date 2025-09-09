c593fda9518022057ce086a47794edb418e41296
sojorepo <thepoorgamer2424@gmail.com>
2025-09-10 07:38:36 +0800

    Upload via drag-and-drop tool

---
+++ 529520.lua
@@ -0,0 +1,37 @@
+addappid(529520)
+
+addappid(529521, 1, "441ea9ee5fd0d670bb8960d7907481cd281cd2d45c7a54c59e38fb5898d2f209")
+
+setManifestid(529521, "1636212985400482488", 5948674226)
+
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+
+setManifestid(228990, "1829726630299308803", 102931551)
+
+addappid(884760)
+
+addappid(884761)
+
+addappid(884762)
+
+addappid(884860)
+
+addappid(1001280)
+
+addappid(1167880)
+
+addappid(1217840)
+
+addappid(1419950)
+
+addappid(1419951)
+
+addappid(1419952)
+
+addappid(2093190)
+
+addappid(2093230)
+
+addappid(2093231)
+
+addappid(2093232)

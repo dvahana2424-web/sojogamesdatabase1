419dba32654081f04577c17ae2059b4eb351199b
sojorepo <thepoorgamer2424@gmail.com>
2025-09-23 20:06:34 +0800

    Upload via drag-and-drop tool

---
+++ 3818380.lua
@@ -0,0 +1,13 @@
+addappid(3818380)
+
+addappid(3818381, 1, "fe8e7c22c12ac0a8f52723b31eeb6f08177daf522701d00bc1cfe20cfb4eb2e3")
+
+setManifestid(3818381, "4558899691402809721", 269799326)
+
+addappid(3818410)
+
+addappid(3818420)
+
+addappid(3818430)
+
+addappid(3818440)

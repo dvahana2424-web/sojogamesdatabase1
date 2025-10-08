c55f9be568166b1f69d9da29e90f75f06139a444
dvahana2424-web <dvahana2424@gmail.com>
2025-10-08 19:33:30 +0800

    Upload via drag-and-drop tool

---
+++ 3916620.lua
@@ -0,0 +1,17 @@
+addappid(3916620)
+
+addappid(3916621, 1, "39d35a7dbb8062822e1817e486a8efbb2b6d7c56b12d1e8f3eafe8f447702413")
+
+setManifestid(3916621, "3265842162246855869", 1022248292)
+
+addappid(3916630)
+
+addappid(3916640)
+
+addappid(3916650)
+
+addappid(4022060)
+
+addappid(4061930)
+
+addappid(4063730)

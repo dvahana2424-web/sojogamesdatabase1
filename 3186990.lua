c86e4849a19d7eedcd1dddc0a9e2eafeee7c9b5f
dvahana2424-web <dvahana2424@gmail.com>
2026-07-20 08:17:39 +0800

    Add files via upload

---
+++ 3186990.lua
@@ -0,0 +1,12 @@
+-- 3186990's Lua and Manifest Created by Hubcap Manifest
+-- Carrot Catcher
+-- Created: July 19, 2026 at 19:58:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3186990) -- Carrot Catcher
+-- MAIN APP DEPOTS
+addappid(3186991, 1, "ca29b5e6ec49611557a27f1b4f4d1953b1442116b635250c8019e7d675f891b9") -- Depot 3186991
+setManifestid(3186991, "4704047985175989252", 42197783)

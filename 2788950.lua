89b20ebb2918a44c6afb39309f04a377c0f2fa2a
dvahana2424-web <dvahana2424@gmail.com>
2026-07-20 08:19:39 +0800

    Add files via upload

---
+++ 2788950.lua
@@ -0,0 +1,14 @@
+-- 2788950's Lua and Manifest Created by Hubcap Manifest
+-- Carrot Survivors
+-- Created: July 19, 2026 at 19:58:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2788950) -- Carrot Survivors
+-- MAIN APP DEPOTS
+addappid(2788952, 1, "de0857f952b80a6773ed082d95f0e41c4d8ad16e3b30431e3f5fe3cff8ac6854") -- Depot 2788952
+setManifestid(2788952, "5258512999455866046", 184614439)
+addappid(2788953, 1, "dafe00e8e8f001a8d305adfc6245cb45b3fa2d55029c40f1d7b830862c388db5") -- Depot 2788953
+setManifestid(2788953, "500586095901660307", 175649227)

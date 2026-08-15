a030247ac9a43cb4e705b378fbe3ee9f7ee57a7b
dvahana2424-web <dvahana2424@gmail.com>
2026-08-15 16:28:15 +0800

    Update 2001760.lua

--- 2001760.lua
+++ 2001760.lua
@@ -1,15 +1,5 @@
--- Generated with Luie @ https://lua.tools/
--- 2001760 - Beast of Reincarnation
--- Generated 2026-08-01 19:31:23 UTC
--- # Depots (Total/DLC/Shared): 3/0/2
-
--- Main AppID
 addappid(2001760)
-
--- Main Depots
 addappid(2001761, 1, "0d843c3fd4bba4400b829693c665e6394ad8311f3f7f6c0acf31c71eab887c0c")
 setManifestid(2001761, "6589835941758702869", 34142153981)
-
--- Shared Depots (Runtimes / Launchers / ETC)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
